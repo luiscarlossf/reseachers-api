a113435e01246674b70f895b269ce1bff8cef988
Luis <luiscarlos.sf2017@gmail.com>
2020-04-09 01:20:49 -0300

    Adiciona o modelo Projeto

---
+++ models/projeto.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+
+//Definindo o esquema
+var Schema = mongoose.Schema;
+
+var ProjetoSchema = new Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    auxilios: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Instituicao'
+    }],
+    dataInicio: {
+        type: Date,
+        require:true
+    },
+    dataFim: {
+        type: Date,
+        require:true
+    },
+    pesquisadores: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Pesquisador'
+    }],
+    linhas: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Linha'
+    }],
+    descricao: {
+        type: String,
+        required: true
+    }
+});
+
+//Virtual para finalizado do Projeto, indica se o projeto já foi finalizado
+ProjetoSchema.virtual('finalizado').get(function(){
+    return Date.now > this.dataFim;
+});
+
+//Virtual para URL do Projeto
+ProjetoSchema.virtual('url').get(function(){
+    return '/projeto/' + this._id;
+});
+
+//Exportando o modelo Projeto
+module.exports = mongoose.model('Projeto', ProjetoSchema);

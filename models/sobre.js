d064360d1c11a63ea2273a61adffe3c469455bdb
Luis <luiscarlos.sf2017@gmail.com>
2020-04-09 01:20:18 -0300

    Adicona o modelo Sobre.

---
+++ models/sobre.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose');
+
+//Definindo o esquema
+var Schema = mongoose.Schema;
+
+var SobreSchema = new Schema({
+    nome:{
+        type: String, 
+        required: true
+    },
+    sigla: {
+        type: String, 
+        required: true
+    },
+    logoPath: {
+        type: String
+    },
+    imagemPath: {
+        type: String
+    },
+    descricao: {
+        type: String, 
+        required: true
+    },
+    telefone: {
+        type: String, 
+        maxlength: 20
+    },
+    email: {
+        type: String
+    },
+    endereco: {
+        type: String
+    },
+    facebook: {
+        type: String
+    },
+    twitter: {
+        type: String
+    },
+    instagram: {
+        type: String
+    }
+});
+
+//Virtual para URL do Sobre
+SobreSchema.virtual('url').get(function(){
+    return '/sobre/' + this._id;
+});
+
+//Exportando o modelo Sobre
+module.exports = mongoose.model('Sobre', SobreSchema);

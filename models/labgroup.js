f4c895b262b3f4e1dab12ddcbbf0be3aee28d687
Luis <luiscarlos.sf2017@gmail.com>
2020-04-09 01:21:37 -0300

    Adiciona o modelo LagGroup

---
+++ models/labgroup.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose');
+
+//Definindo o esquema
+var Schema = mongoose.Schema;
+
+var LabGroupSchema = new Schema({
+    link:{
+        type: String,
+        required: true
+    },
+    tipo:{
+        type: String,
+        required: true,
+        enum: ['Laboratório de Pesquisa', 'Grupo de Pesquisa', 'Núcleo de Pesquisa']
+    },
+    
+    responsavel: {
+        type: Schema.Types.ObjectId,
+        ref: 'Pesquisador'
+    },
+    idioma: {
+        type: Schema.Types.ObjectId,
+        ref: 'Idioma'
+    },
+    tema:{
+        type: Schema.Types.ObjectId,
+        ref: 'Tema',
+    },
+    sobre:{
+        type: Schema.Types.ObjectId,
+        ref: 'Sobre',
+    },
+    publicacoes:[{
+        type: Schema.Types.ObjectId,
+        ref: 'Publicacao',
+    }],
+    projetos:[{
+        type: Schema.Types.ObjectId,
+        ref: 'Projeto',
+    }]
+});
+
+//Virtual para URL do LabGroup
+LabGroupSchema.virtual('url').get(function(){
+    return '/labgroup/' + this._id;
+});
+
+//Exportando o modelo LabGroup
+module.exports = mongoose.model('LabGroup', LabGroupSchema);

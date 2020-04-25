1f5ae0175676ced1e64115a951625988f9a7cf95
Luis <luiscarlos.sf2017@gmail.com>
2020-04-25 14:23:19 -0300

    Modifica os controllers do recurso Idioma

--- controllers/idiomaController.js
+++ controllers/idiomaController.js
@@ -4,9 +4,10 @@ var async = require('async');
 
 //Retorna uma lista de todos os idiomas
 exports.idiomaList = function(req, res, next){
-    var offset = 0;
-    var limit = 1000;
-    var sort = {codigo: 'asc'};
+    var offset = 0; //A lista de recursos inicia do 0
+    var limit = 1000; //Limite máximo de recursos retornados.
+    var sort = req.query.sort? req.query.sort : {codigo: 'asc'}; //Padrão de ordenação
+
     if(req.query.offset){
         var offset = parseInt(req.query.offset);
     }
@@ -15,8 +16,9 @@ exports.idiomaList = function(req, res, next){
     }
     req.accepts('json');
     Idioma.find()
-    .limit(limit) 
     .sort(sort)
+    .skip(offset)
+    .limit(limit) 
     .exec(function (error, lista_idioma){
         if(error){return next(error);}
         res.status(200).json(lista_idioma);
@@ -26,21 +28,19 @@ exports.idiomaList = function(req, res, next){
 //Adiciona um novo idioma
 exports.idiomaCreate = function(req, res, next){
     req.accepts('json');
-
-    body = new Object(req.body);
-    if (Object.keys(body).length !== 0){
-        idioma = new Idioma(body);
+    if (Object.entries(req.body).length){
+        idioma = new Idioma(req.body);
         idioma.save(function (error){
             if (error){
                 next(error);
             }
             idiomaURI = uri.getURI(req, 'idiomas', idioma.id);
             res.set('Location', idiomaURI)
-            .status(200)
-            .json(idioma.toJSON());
+            .status(201)
+            .json(idioma);
         });
     }else{
-        res.status(400).json({"codigo": 400, "error": "A requisição requer um elemento JSON no body."});
+        res.status(400).json({"codigo": 400, "error": "A requisição requer um elemento a ser criado deve ser representado por um objeto JSON no body."});
     }  
 };
 
@@ -56,6 +56,31 @@ exports.idiomaDetail = function(req, res, next){
     });
 };
 
+//Atualiza as informações do idioma
+exports.idiomaUpdatePut = function(req, res, next){
+    req.accepts('json');
+    if (Object.entries(req.body).length){
+        if(('id' in req.body) || ('_id' in req.body)){
+            res.status(406).json({"code": 406, "error" : "É impossível atualizar o identificador de um recurso, se você deseja adicionar um novo recurso use o método POST."});
+        }
+        codigo = req.body.codigo;
+        nome = req.body.nome;
+        imagePath = req.body.imagePath;
+        Idioma.findByIdAndUpdate(req.params.id,{codigo: codigo, nome: nome, imagePath: imagePath}, {new: true}, function (error, idioma){
+            if(error){
+                next(error);
+            }else{
+                idiomaURI = uri.getURI(req, 'idiomas', idioma.id);
+                res.set('Location', idiomaURI)
+                .status(200)
+                .json(idioma);
+            }
+        });
+    }else{
+        res.status(400).json({"code" : 400, "error" : "A requisição veio sem body, porque coloque o objeto JSON no corpo da requisição."});
+    }
+};
+
 //Exclui um idioma
 exports.idiomaDelete = function(req, res, next){
     req.accepts('json');
@@ -68,49 +93,21 @@ exports.idiomaDelete = function(req, res, next){
     });
 };
 
-//Atualiza as informações do idioma
-exports.idiomaUpdatePut = function(req, res, next){
-    req.accepts('json');
-    body = new Object(req.body);
-    console.log(Object.keys(body));
-    if (Object.keys(body).length !== 0){
-        if(('id' in body) || ('_id' in body)){
-            res.status(406).json({"code": 406, "error" : "É impossível atualizar o identificador de um recurso, se você deseja adicionar um novo recurso use o método POST."});
-        }
-        codigo = body.codigo;
-        nome = body.nome;
-        imagePath = body.imagePath;
-        Idioma.findByIdAndUpdate(req.params.id,{codigo: codigo, nome: nome, imagePath: imagePath}, {new: true}, function (error, idioma){
-            if(error){
-                next(error);
-            }else{
-                idiomaURI = uri.getURI(req, 'idiomas', idioma.id);
-                res.set('Location', idiomaURI)
-                .status(200)
-                .json(idioma.toJSON());
-            }
-        });
-    }else{
-        res.status(400).json({"code" : 400, "error" : "A requisição veio sem body, porque coloque o objeto JSON no corpo da requisição."});
-    }
-};
-
 //Atualia informações específicas do idioma
 exports.idiomaUpdatePatch = function(req, res, next){
     req.accepts('json');
-    body = new Object(req.body);
-    if (Object.keys(body).length !== 0){
-        if(('id' in body) || ('_id' in body)){
+    if (Object.keys(req.body).length){
+        if(('id' in req.body) || ('_id' in req.body)){
             res.status(406).json({"code": 406, "error" : "É impossível atualizar o identificador de um recurso, se você deseja adicionar um novo recurso use o método POST."});
         }
-        Idioma.findByIdAndUpdate(req.params.id,body,{new: true}, function (error, idioma){
+        Idioma.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (error, idioma){
             if(error){
                 next(error);
             }else{
                 idiomaURI = uri.getURI(req, 'idiomas', idioma.id)
                 res.set('Location', idiomaURI)
                 .status(200)
-                .json(idioma.toJSON());
+                .json(idioma);
             }
         });
     }else{
